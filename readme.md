705fe4c82bbb23cb2376805c1c1868d448676edc
Hilmi AM <imiia75775@proton.me>
2024-09-09 15:31:25 +0700

    request decorator

--- readme.md
+++ readme.md
@@ -11,3 +11,10 @@ Controller declarative menggunakan Annotation/decorator
 buat controller `nest generate controller name path`
 
 routing di controller pake decorator di method nya..
+
+request default using express js if want using express.Request harus pake @Req() untuk pake object Request dari Express
+
+rekomendasi nya langsung nyebutin data yang di butuhkan di param menggunakan decorator yang di sediakan nest js
+
+always using request decorator from nestjs
+tapi ada yang perlu di pertanyakan kira kira? si nest ini nerima data nya default nya apa ya? bisa conversi otomatis atau harus bikin sendiri hmmmm
--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,31 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/hello-r') // get query params id
+  getQueryRecommend(
+    @Query('name') name: string,
+    @Query('location') location: string,
+  ): string {
+    return `hello ${name ?? 'Guest'} from ${location || 'ID'}`;
+  }
+
+  @Get('/i/:idUser') // get params id
+  getByIdRecommend(@Param('idUser') id: string): string {
+    return `GET by ID Recommended: ${id}`;
+  }
+
+  @Get('/hello') // get query params name
+  getQueryNotRecommend(@Req() request: Request): string {
+    return `GET query by name: ${request.query.name}`;
+  }
+
+  @Get('/:idUser') // get params id
+  getByIdNotRecommend(@Req() request: Request): string {
+    return `GET by ID: ${request.params.idUser}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
